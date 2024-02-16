d5fc48aa806a81dbec5b2f21869de4cd5058c02d
Berkant Karaca <bkaraca.71m@gmail.com>
2024-02-17 00:55:26 +0300

    Admin paneli yetenekler kısmı yapıldı.

---
+++ Core_Proje/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar() //Sol menü burada tutulacak
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter() 
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavigationPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Core_Proje/Controllers/SkillController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Yetenek Listesi";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Listesi";
+
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            ViewBag.v1 = "Yetenek Ekleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Ekleme";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index"); //index aksiyonuna yönlendirir.
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values = skillManager.GetByID(id);
+            skillManager.TDelete(values); //silme işlemini yapmak için ilk önce o nesneyi bulmam lazım.
+            return RedirectToAction("Index");
+        }
+        [HttpGet] //sayfa yüklendiği zaman çalışır
+        public IActionResult EditSkill(int id)
+        {
+            ViewBag.v1 = "Düzenleme";
+            ViewBag.v2 = "Yetenekler";
+            ViewBag.v3 = "Yetenek Güncelleme";
+            var values = skillManager.GetByID(id);
+            return View(values);
+        }
+
+        [HttpPost] //sayfa yüklendiği zaman çalışır
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
+    }
+}
