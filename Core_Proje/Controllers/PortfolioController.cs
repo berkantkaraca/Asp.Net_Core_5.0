be82b65d46e65feedd753fa862d0135232e6d949
Berkant Karaca <bkaraca.71m@gmail.com>
2024-03-03 12:40:06 +0300

    Admin paneli portfolyo kısmı yapıldı.

---
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Listesi";
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+        
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekleme";
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
+        }
+    }
+}
