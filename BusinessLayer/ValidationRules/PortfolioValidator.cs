98847d3f89d525cfa87a4a2e6efa599ceec20e8f
Berkant Karaca <bkaraca.71m@gmail.com>
2024-03-04 10:31:21 +0300

    Portfolio için fluent validation tanımlandı.

---
+++ BusinessLayer/ValidationRules/PortfolioValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class PortfolioValidator:AbstractValidator<Portfolio>
+    {
+        //Kurallar Constructer methot içine yazılır.
+        public PortfolioValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Proje adı boş geçilemez");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel alanı adı boş geçilemez");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Görsel 2 alanı adı boş geçilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat alanı adı boş geçilemez");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("Değer alanı adı boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Proje adı en az 5 karakter olmak zorundadır");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("Proje adı en fazla 100 karakter olmak zorundadır");
+        }
+    }
+}
--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Controllers
@@ -16,20 +18,73 @@ namespace Core_Proje.Controllers
             var values = portfolioManager.TGetList();
             return View(values);
         }
-        
+
         [HttpGet]
         public IActionResult AddPortfolio()
         {
+            ViewBag.v1 = "Proje Listesi";
+            ViewBag.v2 = "Projeler";
+            ViewBag.v3 = "Proje Ekleme";
             return View();
         }
         [HttpPost]
         public IActionResult AddPortfolio(Portfolio portfolio)
         {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(portfolio); //parametreyi valide eder
+
+            if (results.IsValid)
+            {
+                portfolioManager.TAdd(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetByID(id);//silme işlemini yapmak için ilk önce o nesneyi bulmam lazım.
+            portfolioManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet] //sayfa yüklendiği zaman çalışır
+        public IActionResult EditPortfolio(int id)
+        {
             ViewBag.v1 = "Proje Listesi";
             ViewBag.v2 = "Projeler";
             ViewBag.v3 = "Proje Ekleme";
-            portfolioManager.TAdd(portfolio);
-            return RedirectToAction("Index");
+            var values = portfolioManager.TGetByID(id);
+            return View(values);
         }
+
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validations = new PortfolioValidator();
+            ValidationResult results = validations.Validate(portfolio); //parametreyi valide eder
+
+            if (results.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
     }
 }
